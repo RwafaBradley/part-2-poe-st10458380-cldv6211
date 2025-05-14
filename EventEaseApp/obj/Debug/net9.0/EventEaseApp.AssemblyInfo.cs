42221f86e8bb7e48a5489532f55dea79e2b94522
RwafaBradley <st10458380@imconnect.edu.za>
2025-05-14 16:43:19 +0200

    part2

--- EventEaseApp/obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
+++ EventEaseApp/obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60075056239825ba52467c3ffe9e31a989abb594")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
